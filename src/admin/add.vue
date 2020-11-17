4fbd61e9bf28fb43f71324c186fbde929f28040b
ST-AR15 <2862159026@qq.com>
2020-11-17 13:15:46 +0800

    add basic elements for add.vue

--- src/admin/add.vue
+++ src/admin/add.vue
@@ -1,15 +1,61 @@
 <template>
     <div class="add" id="add">
-        <p>新增题目</p>
+        <h2>新增题目</h2>
+        <ul>
+            <li>
+                <span>题目名称</span>
+                <a-input :value="name"></a-input>
+            </li>
+            <li>
+                <span>时限</span>
+                <a-input 
+                    :value="timeLimit"
+                    suffix="ms"
+                ></a-input>
+            </li>
+            <li>
+                <span>存限</span>
+                <a-input
+                    :value="storageLimit"
+                    suffix="MB"
+                ></a-input>
+            </li>
+            <li>
+                <span>题目类型</span>
+                <a-radio-group v-model="QType">
+                    <a-radio value="normal">普通验证</a-radio>
+                    <a-radio value="special">特别验证</a-radio>
+                </a-radio-group>
+            </li>
+            <li>
+                <span>题目内容</span>
+                <a-textarea
+                    :rows="4"
+                ></a-textarea>
+            </li>
+            <li>
+                <a-button type="primary">上传</a-button>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            // 题目名称
+            name: "",
+            timeLimit: 1000,
+            storageLimit: 128,
+            QType: "normal",
+        }
+    }
 }
 </script>
 
 <style>
-
+    ul {
+        list-style: none;
+    }
 </style>

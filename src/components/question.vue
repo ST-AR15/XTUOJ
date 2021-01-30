474efd534a4514b93fbac2eeaf682e8d4779a97d
ST-AR15 <2862159026@qq.com>
2021-01-30 23:05:43 +0800

    question页面的评论（没做完，打算直接套antd组件了）

--- src/components/question.vue
+++ src/components/question.vue
@@ -38,14 +38,75 @@
                         <mavon-editor v-model="question.questionDetail" :subfield="false" :toolbarsFlag="false" defaultOpen="preview"></mavon-editor>
                     </section>
                     <section v-show="leftInner[0] == 'discuss'" class="discussSection">
-                        
+                        <div class="comment">
+                            <mavon-editor v-model="comment" :toolbars="commentToolbar" :subfield="false" placeholder="想说什么，尽管说吧~"></mavon-editor>
+                        </div>
+                        <div class="context">
+                            <div class="contextInner" v-for="data in commentContext" v-bind:key="data.Rid">
+                                <div class="contextItem">
+                                    <div class="contextHeader">
+                                        <span v-text="data.Uid"></span>
+                                        <span v-text="data.time"></span>
+                                    </div>
+                                    <div class="contextMain">
+                                        <p v-text="data.context"></p>
+                                    </div>
+                                    <div class="contextFooter">
+                                        <a-space>
+                                            <span 
+                                                v-if="commentPost.find(o => o.postId == data.Rid)"
+                                                v-text="`${ !data.isRecomment? '查看':'收起' }${ commentPost.find(o => o.postId == data.Rid).comment.length }条回复`"
+                                                @click="data.isRecomment = !data.isRecomment">
+                                            </span>
+                                            <span
+                                                @click="data.isReply = !data.isReply"
+                                                v-text="`${ !data.isReply? '回复':'收起回复' }`"
+                                            ></span>
+                                        </a-space>
+                                    </div>
+                                </div>
+                                <div class="contextTextarea" v-show="data.isReply">
+                                    <mavon-editor v-model="commentReply" :toolbars="commentToolbar" :subfield="false" placeholder="想说什么，尽管说吧~"></mavon-editor>
+                                    <a-space style="margin-top: 5px; text-align: right">
+                                        <a-button type="primary">确定</a-button>
+                                        <a-button type="primary">取消</a-button>
+                                    </a-space>
+                                </div>
+                                <div class="reComment" v-show="data.isRecomment">
+                                    <div class="contextItem">
+                                        <div class="contextHeader">
+                                            <span v-text="data.Uid"></span>
+                                            <span v-text="data.time"></span>
+                                        </div>
+                                        <div class="contextMain">
+                                            <p v-text="data.context"></p>
+                                        </div>
+                                        <div class="contextFooter">
+                                            <span>回复</span>
+                                        </div>
+                                    </div>
+                                    <div class="contextItem">
+                                        <div class="contextHeader">
+                                            <span v-text="data.Uid"></span>
+                                            <span v-text="data.time"></span>
+                                        </div>
+                                        <div class="contextMain">
+                                            <p v-text="data.context"></p>
+                                        </div>
+                                        <div class="contextFooter">
+                                            <span>回复</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                     <section v-show="leftInner[0] == 'submit'" class="submitSection"></section>
                 </div>
                 <div class="buttons">
                     <a-space>
-                        <a-input></a-input>
-                        <a-button>跳转</a-button>
+                        <a-input placeholder="请输入题号"></a-input>
+                        <a-button type="primary">跳转</a-button>
                     </a-space>
                 </div>
             </div>
@@ -109,7 +170,10 @@ export default {
     },  // 从父组件获得题目ID，然后在接口里获得全部值
     data() {
         return {
-            leftInner: ["question"],
+            leftInner: ["question"],  // 左边显示的内容
+            comment: "评论",   // 编辑框内容
+            commentReply: "回复",  // 回复框内容
+            commentRip: 0,   // 回复对象的ID
             leftW: 500,   // 左边宽度
             rightW: 500,   // 右边宽度
             loading: true, // 加载状态
@@ -131,6 +195,156 @@ export default {
                 tips: [],
                 questionDetail: "请稍候……",
                 code: "",  //当前输入的代码
+            },
+            commentContext: [
+                {
+                    Rid: 1,
+                    Uid: 201705551222,
+                    Pid: 1006,
+                    context: "谢谢",
+                    postId: null,
+                    time: "2020-12-29 09:04:28",
+                    ip: null,
+                    isRecomment: false,
+                    isReply: false,
+                },
+                {
+                    Rid: 5,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                    isRecomment: false,
+                },
+                {
+                    Rid: 6,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                },
+                {
+                    Rid: 7,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                },
+                {
+                    Rid: 8,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                },
+                {
+                    Rid: 9,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                },
+                {
+                    Rid: 15,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                },
+                {
+                    Rid: 25,
+                    Uid: 2077,
+                    Pid: 1006,
+                    context: "你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了你马没了",
+                    postId: null,
+                    time: "2020-12-29 09:07:28",
+                    ip: null,
+                },
+            ],
+            commentPost: [
+                {
+                    postId: 1,
+                    comment: [
+                        {
+                            Rid: 3,
+                            Uid: 2010,
+                            Pid: 1006,
+                            context: "我谢你马",
+                            time: "2020-12-29 09:05:22",
+                            ip: null,
+                        },
+                        {
+                            Rid: 31,
+                            Uid: 2010,
+                            Pid: 1006,
+                            context: "你马没了",
+                            time: "2020-12-29 19:05:22",
+                            ip: null,
+                        }
+                    ]
+                    
+                },
+                {
+                    postId: 5,
+                    comment: [
+                        {
+                            Rid: 123,
+                            Uid: 2010,
+                            Pid: 1006,
+                            context: "我谢你马",
+                            time: "2020-12-30 09:05:22",
+                            ip: null,
+                        },
+                    ]
+                    
+                },
+            ],
+            commentToolbar: {
+                bold: true, // 粗体
+                italic: true, // 斜体
+                header: false, // 标题
+                underline: true, // 下划线
+                strikethrough: true, // 中划线
+                mark: false, // 标记
+                superscript: false, // 上角标
+                subscript: false, // 下角标
+                quote: true, // 引用
+                ol: true, // 有序列表
+                ul: true, // 无序列表
+                link: false, // 链接
+                imagelink: false, // 图片链接
+                code: true, // code
+                table: true, // 表格
+                fullscreen: true, // 全屏编辑
+                readmodel: true, // 沉浸式阅读
+                htmlcode: false, // 展示html源码
+                help: true, // 帮助
+                /* 1.3.5 */
+                undo: true, // 上一步
+                redo: true, // 下一步
+                trash: true, // 清空
+                save: false, // 保存（触发events中的save事件）
+                /* 1.4.2 */
+                navigation: false, // 导航目录
+                /* 2.1.8 */
+                alignleft: false, // 左对齐
+                aligncenter: false, // 居中
+                alignright: false, // 右对齐
+                /* 2.2.1 */
+                subfield: false, // 单双栏模式
+                preview: true, // 预览
             }
             
         }
@@ -263,6 +477,57 @@ export default {
 .question .questionSection > p {
     font-size: 20px;
 }
+.question .discussSection {
+    padding: 0 20px;
+}
+.question .discussSection .context {
+    margin-top: 20px;
+}
+.question .discussSection .contextInner {
+    width: 100%;
+    min-height: 100px;
+    margin: 10px 0;
+    border-bottom: 1px solid #666666;
+}
+.question .discussSection .reComment {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #777777;
+    box-shadow: inset 0 1px 6px #999999;
+    border-radius: 12px;
+    padding: 0 5%;
+    margin: 5px 0;
+}
+.question .discussSection .contextTextarea {
+    margin-bottom: 5px;
+}
+.reComment > .contextItem {
+    border-bottom: 1px solid #666666;
+}
+.question .discussSection .contextItem {
+    width: 100%;
+    min-height: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.question .discussSection .contextHeader {
+    height: 30px;
+    line-height: 30px;
+    display: flex;
+    justify-content: space-between;
+}
+.question .discussSection .contextFooter {
+    height: 30px;
+    line-height: 30px;
+}
+.question .contextFooter span {
+    cursor: pointer;
+}
+.question .contextFooter span:hover {
+    color: #1890FF;
+}
+
 .question .buttons {
     width: 100%;
     height: 64px;

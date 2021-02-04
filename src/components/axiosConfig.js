1765d85c918649eb6b248724e971183246503ccb
ST-AR15 <2862159026@qq.com>
2021-02-04 22:10:25 +0800

    axios格式修改

--- src/components/axiosConfig.js
+++ src/components/axiosConfig.js
@@ -2,16 +2,11 @@ import axios from 'axios'
 // import Vue from 'vue'
 import { notification } from 'ant-design-vue'
 axios.defaults.timeout = 10000;
-// TODO 全部采用这种baseURL写法
 axios.defaults.baseURL = "http://47.106.68.121:8887"
 axios.defaults.headers.common['Authorization'] = "Bearer" + sessionStorage.getItem('token');
-// const baseUrl = "http://172.22.114.116"  // 校园网环境
-// const baseUrl = "http://47.106.68.121:8887"          // 公网url
-// Vue.prototype.$baseUrl = baseUrl
-// Vue.prototype.$bearer = "Bearer"
+
 
 // 请求拦截器，请求发生前发生的事
-// TODO: 两个拦截器的icon都不对
 axios.interceptors.request.use((req) => {
     req.headers["Authorization"] = "Bearer" + sessionStorage.getItem('token'); 
     return req;
@@ -31,7 +26,7 @@ axios.interceptors.response.use((res) => {
 },error => {
     notification.open({
         message: error.message,
-        description: `响应错误，请检查自己的网络连接或联系管理员。url:${ error.config.url }；res:${ error.response.data.message }`,
+        description: `响应错误，请检查自己的网络连接或联系管理员。url:${ error.config.url }；res:${ error.response.data.data }`,
         duration: 0,
         icon: '×',
     });

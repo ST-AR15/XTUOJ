e099356733f7dfe7a8aaba85b9f4f2728acd81e4
ST-AR15 <2862159026@qq.com>
2021-02-26 16:53:13 +0800

    problem

--- src/user/home.vue
+++ src/user/home.vue
@@ -1,45 +1,13 @@
 <template>
-    <div class="home" id="home">
-        <questionlist :buttons="buttons" @goQuestion="goQuestion" />
-    </div>
+    <div class="problems" id="problems">problems</div>
 </template>
 
 <script>
 export default {
-    components: {
-        questionlist,
-    },
-    data() {
-        return {
-            buttons: [
-                {
-                    text: "查看",
-                    method: "goQuestion"
-                }
-            ],
-        }
-    },
-    methods: {
-        // 打开题目页面，传入题目ID
-        goQuestion(param) {
-            // 修改路由到question
-            console.log(param);
-            this.$router.push('/question/' + param.ID);
-        },
-    }
+
 }
 </script>
 
 <style>
-    .home * {
-        font-size: 15px;
-        letter-spacing: 1px;
-    }
-    .home .ant-table-title {
-        padding: 10px 20px;
-    }
-    .highlight {
-        background-color: rgb(255, 192, 105);
-        padding: 0px;
-    }
+
 </style>

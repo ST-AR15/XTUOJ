1f38a6b7ad98662039764a09b4594f414ba7fe7c
ST-AR15 <2862159026@qq.com>
2021-03-16 20:31:19 +0800

    修改了样式和默认内容

--- src/views/user/Home.vue
+++ src/views/user/Home.vue
@@ -4,7 +4,7 @@
             <h2 style="font-size: 22px">通知</h2>
         </a-layout-header>
         <a-layout>
-            <a-layout-sider style="padding: 20px;margin: 10px; border-right: 1px solid #888888">
+            <a-layout-sider style="padding: 20px;margin: 10px; background-color: #FFFFFF">
                 <a-timeline>
                     <a-timeline-item v-for="(item, i) in timer" :key="i" :color="item.color">
                         <p style="margin: 0">{{ item.time }}</p>
@@ -12,10 +12,11 @@
                     </a-timeline-item>
                 </a-timeline>
             </a-layout-sider>
-            <a-layout-content style="padding: 0 10px">
+            <a-layout-content style="padding: 0 20px; background-color: #FFFFFF; margin: 10px">
                 <a-list item-layout="horizontal" :data-source="announcements">
                     <a-list-item slot="renderItem" slot-scope="item">
-                        <a-list-item-meta :description="item.description">
+                        <a-list-item-meta>
+                            <span slot="description" v-html="item.description"></span>
                             <span slot="title">{{ item.title }}</span>
                         </a-list-item-meta>
                         <span>{{ item.time }}</span>
@@ -33,60 +34,55 @@ export default {
             timer: [
                 {
                     color: 'green',
-                    time: "2021-03-04",
-                    content: "考试",
+                    time: "2020-11-13",
+                    content: "项目创建",
                 },
                 {
                     color: 'red',
-                    time: "2021-03-05",
-                    content: "考试"
+                    time: "2021-01-01",
+                    content: "项目遇到了挫折，进行调整"
                 },
                 {
                     color: 'blue',
-                    time: "2021-03-06",
-                    content: "考试",
+                    time: "2021-03-31",
+                    content: "项目中期检查",
                 },
                 {
-                    color: 'green',
-                    time: "2021-03-07",
-                    content: "考试",
-                },
-                {
-                    color: 'green',
-                    time: "2021-03-08",
-                    content: "考试",
+                    color: 'red',
+                    time: "2021-05-25",
+                    content: "答辩",
                 },
             ],
             announcements: [
                 {
                     time: "2021-03-14",
-                    title: '考试',
-                    description: "2021届网络工程期末考试",
+                    title: '最新添加题目1007！',
+                    description: "试着做一做<a href='http://172.25.25.215:8080/#/problems/1007'>1007题</a>吧~",
                 },
                 {
                     time: "2021-03-14",
-                    title: "考试",
-                    description: "2021届网络工程期末考试",
+                    title: '湘大OJ正式开放！',
+                    description: "湘大自主研发的OJ已经开放了，大家愉快地使用吧~",
                 },
                 {
                     time: "2021-03-14",
-                    title: "考试",
-                    description: "2021届网络工程期末考试",
+                    title: "联系我们",
+                    description: "湘大新OJ前端制作者<a href='tel:13975703258'>电话号码13975703258</a>，<a href='mailto:slighttt@qq.com'>邮箱slighttt@qq.com</a>欢迎联系~",
                 },
                 {
                     time: "2021-03-14",
-                    title: "考试",
-                    description: "2021届网络工程期末考试",
+                    title: "意见与建议",
+                    description: "请联系上面的电话和邮箱~",
                 },
                 {
                     time: "2021-03-14",
-                    title: "考试",
-                    description: "2021届网络工程期末考试",
+                    title: "开源",
+                    description: "本前端项目开源于<a target='_blank' href='http://www.github.com/ST-AR15/XTUOJ'>github</a>",
                 },
                 {
                     time: "2021-03-14",
-                    title: "考试",
-                    description: "2021届网络工程期末考试",
+                    title: "证书",
+                    description: "该项目还没申请证书~",
                 },
             ],
         }

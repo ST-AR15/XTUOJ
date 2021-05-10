3b868d46f90d08fdcf3d05f360d5584e388a0ea2
ST-AR15 <2862159026@qq.com>
2021-05-10 11:15:58 +0800

    优化了页面切换时的体验,现在切换页面后会默认回到顶部

--- src/router/index.js
+++ src/router/index.js
@@ -142,7 +142,14 @@ const routes = [
 ]
 
 const router = new VueRouter({
-    mode: 'history',
+    // mode: 'history',
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return {x: 0, y: 0}
+        }
+    },
     // base: process.env.BASE_URL,
     routes: routes
 })

aa5df0d4a55c66b8cc455cafcaa4e199a3cd8a2a
ST-AR15 <2862159026@qq.com>
2021-05-10 16:51:03 +0800

    完善了题目创建,现在能通过压缩文件批量创建题目了

--- src/views/admin/question/Add.vue
+++ src/views/admin/question/Add.vue
@@ -2,7 +2,31 @@
 <template>
     <div class="admin-question-add" id="admin-question-add">
         <h2 class="admin-title">新增题目</h2>
-        <questionform ref="questionForm" okText="创建" @querySubmit="queryForm" />
+        <a-tabs default-active-key="one"  @change="handleTab">
+            <a-tab-pane key="one">
+            <span slot="tab">
+                单个题目添加
+            </span>
+                <questionform ref="questionForm" okText="创建" @querySubmit="queryForm" />
+            </a-tab-pane>
+            <a-tab-pane key="file">
+            <span slot="tab">
+                文件批量添加
+            </span>
+                <a-upload
+                    name="file"
+                    :multiple="true"
+                    :action= questionFile.url
+                    :header= questionFile.header
+                    accept=".zip"
+                    @change="handleQuestionFile"
+                >
+                <a-button> <a-icon type="upload" /> 点击上传文件 </a-button>
+            </a-upload>
+            <p>已上传的题目ID:{{ questionFile.result }}</p>
+            </a-tab-pane>
+        </a-tabs>
+        
         <a-modal
             title="添加成功!"
             :visible="modal.visible"
@@ -28,10 +52,32 @@ export default {
         return {
             modal: {
                 visible: false,
+            },
+            questionFile: {
+                url: "",
+                header: {},
+                result: [],
             }
         }
     },
     methods: {
+        handleTab(e) {
+            // 初始化题目上传的upload
+            if(e == 'file') {
+                // 设置有关题目上传的信息
+                this.questionFile.url = this.$axios.defaults.baseURL + '/api/upload/problem',
+                this.questionFile.header = {
+                    Authorization: "Bearer" + this.$store.state.token,
+                }
+            }
+        },
+        handleQuestionFile(info) {  
+            // 先清空
+            this.questionFile.result = [];
+            for(let i in info.fileList) {
+                this.questionFile.result = this.questionFile.result.concat(info.fileList[i].response.data);
+            }
+        },
         queryForm(form) {     //提交表单
             const info = {    // 传给后端的info
                 Tittle: form.name,
